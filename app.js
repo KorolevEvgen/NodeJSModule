08deb94f9f48c76cd3dbfb197cf316cd4a18e3a2
Jenya Korolev <korolev.evgen163@gmail.com>
2022-11-15 23:22:16 +0200

    improved code

--- app.js
+++ app.js
@@ -1,83 +1,52 @@
-const fs = require('node:fs');
-const { misha, masha, marry, anna, liza, maksim } = require('./person/people');
+const fs = require('fs/promises');
+const path = require('path');
+//const { misha, masha, marry, anna, liza, maksim } = require('./person/people');
 
-fs.mkdir('boys', (err) => {
-    console.log(err);
-});
-
-fs.mkdir('girls', (err) => {
-    console.log(err);
-});
+// fs.mkdir('boys', (err) => {
+//     console.log(err);
+// });
 //
+// fs.mkdir('girls', (err) => {
+//     console.log(err);
+// });
+
 // const dataMisha = JSON.stringify(misha);
-// fs.writeFileSync('./boys/misha.json', dataMisha);
-//
-// const dataMasha = JSON.stringify(masha);
-// fs.writeFileSync('./boys/masha.json', dataMasha);
-//
-// const dataMarry = JSON.stringify(marry);
-// fs.writeFileSync('./boys/marry.json', dataMarry);
-//
-// const dataAnna = JSON.stringify(anna);
-// fs.writeFileSync('./girls/anna.json', dataAnna);
-//
-// const dataLiza = JSON.stringify(liza);
-// fs.writeFileSync('./girls/liza.json', dataLiza);
-//
-// const dataMaksim = JSON.stringify(maksim);
-// fs.writeFileSync('./girls/maksim.json', dataMaksim);
+ // fs.writeFileSync('./boys/misha.json', dataMisha);
+ //
+ // const dataMasha = JSON.stringify(masha);
+ // fs.writeFileSync('./boys/masha.json', dataMasha);
+ //
+ // const dataMarry = JSON.stringify(marry);
+ // fs.writeFileSync('./boys/marry.json', dataMarry);
+ //
+ // const dataAnna = JSON.stringify(anna);
+ // fs.writeFileSync('./girls/anna.json', dataAnna);
+ //
+ // const dataLiza = JSON.stringify(liza);
+ // fs.writeFileSync('./girls/liza.json', dataLiza);
+ //
+ // const dataMaksim = JSON.stringify(maksim);
+ // fs.writeFileSync('./girls/maksim.json', dataMaksim);
 
-fs.readdir('./boys', (err, files) => {
-    console.log(files);
+const sorter = async (readFolder, writeFolder, gender) => {
+    try {
+        const folderPath = path.join(__dirname, readFolder);
 
-    for (const fileName of files) {
-        fs.stat(`./boys/${fileName}`, (err1, stats) => {
-            console.log(`./boys/${fileName}`);
-            console.log(stats.isDirectory());
+        const files = await fs.readdir(folderPath);
 
-            if (stats.isFile()) {
-                fs.readFile(`./boys/${fileName}`, (err2, data) => {
-                    const person = JSON.parse(data);
-                    console.log(person.gender);
-                    if (person.gender === 'Female') {
-                        fs.rename(`./boys/${fileName}`, `./girls/${fileName}`, (err) => {
-                            console.log(err);
-                        });
-                    }
-                });
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const data = await fs.readFile(filePath);
+            const user = JSON.parse(data);
 
+            if (user.gender === gender) {
+                await fs.rename(filePath, path.join(__dirname, writeFolder, file));
             }
-        });
+        }
+    } catch (e) {
+        console.error(e)
     }
-});
+}
 
-fs.readdir('./girls', (err, files) => {
-    console.log(files);
-
-    for (const fileName2 of files) {
-        fs.stat(`./girls/${fileName2}`, (err1, stats2) => {
-            console.log(`./girls/${fileName2}`);
-            console.log(stats2.isDirectory());
-
-            if (stats2.isFile()) {
-                fs.readFile(`./girls/${fileName2}`, (err2, data2) => {
-                    const person2 = JSON.parse(data2);
-                    console.log(person2.gender);
-                    if (person2.gender === 'Male') {
-                        fs.rename(`./girls/${fileName2}`, `./boys/${fileName2}`, (err) => {
-                            console.log(err);
-                        });
-                    }
-                });
-
-            }
-        });
-    }
-});
-
-
-
-
-
-
-
+sorter('boys', 'girls', 'Female');
+sorter('girls', 'boys', 'Male');
